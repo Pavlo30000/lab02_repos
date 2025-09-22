e9424713db08a417410da622fb11daefae45e23f
Pavlo30000 <user.pavlo3000@gmail.com>
2025-09-22 23:09:46 +0300

    PR_2.01

---
+++ PR_2.0/PR_2.0.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double x;
+    double z1;
+    /*double z2;*/
+    cout << "x = "; cin >> x;
+    z1 = 2 * x;
+    /*z2 = x + x;*/
+    cout << "z1 = " << z1 << endl;
+    /*cout << "z2 = " << z2 << endl;*/
+    cin.get();
+
+    return(0);
+}
